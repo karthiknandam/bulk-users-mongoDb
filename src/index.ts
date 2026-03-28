72e4bc67337b531ebe1475588b4e1f7bdbe8c1ad
Karthik Nandam <carthicknandam@gmail.com>
2026-03-28 14:47:48 +0530

    fix: update express json file parsing limit to 10mb

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import router from "./routes";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 app.get("/health", (_req: Request, res: Response) => {
   res.json({
